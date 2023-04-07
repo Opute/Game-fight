8f54f73f0d01ded0c5100fb26212dab9f00a5d76
Opute <andreo130201@gmail.com>
2023-04-07 12:23:15 -0300

    Ultima atualizacao

---
+++ src/Arena.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+import Win from "./Win";
+
+export default function arena() {
+
+    return (
+        <div className="Arena">
+          <Win/>
+        </div>
+      );
+}
---
+++ src/Win.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+export default function win() {
+
+    return (
+        <div className="Win">
+          Wins!
+        </div>
+      );
+}
